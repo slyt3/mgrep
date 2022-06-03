d95631353ffb18e24b6bb97623698ccd31f3e9ee
Mantas <mantas.sumin@gmail.com>
2022-06-03 12:39:19 +0300

    first commit

---
+++ worklist/worklist.go
@@ -0,0 +1,33 @@
+package worklist
+
+// Empty path indicates that there are no more jobs to be done
+type Entry struct {
+	Path string
+}
+
+type Worklist struct {
+	jobs chan Entry
+}
+
+func (w *Worklist) Add(work Entry) {
+	w.jobs <- work
+}
+
+func (w *Worklist) Next() Entry {
+	j := <-w.jobs
+	return j
+}
+
+func New(bufSize int) Worklist {
+	return Worklist{make(chan Entry, bufSize)}
+}
+
+func NewJob(path string) Entry {
+	return Entry{path}
+}
+
+func (w *Worklist) Finalize(numWorkers int) {
+	for i := 0; i < numWorkers; i++ {
+		w.Add(Entry{""})
+	}
+}
